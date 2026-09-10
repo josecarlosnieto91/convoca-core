7e0807c827da4f2d1b7e7cbcc59bd4cae62af3d5
Jose Carlos Nieto Ramos <josecarlos@biodevas.org>
2026-09-10 11:26:54 +0200

    test(core): renombra los tests de integracion de Capabilities (colision de clase)

--- tests/Integration/CapabilitiesIntegrationTest.php
+++ tests/Integration/CapabilitiesIntegrationTest.php
@@ -20,7 +20,7 @@ use PHPUnit\Framework\TestCase;
  * @covers ::register
  * @covers ::ensure
  */
-class CapabilitiesTest extends TestCase
+class CapabilitiesIntegrationTest extends TestCase
 {
     /**
      * Test that get_all returns the expected structure.
